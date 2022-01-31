9c443988915d780bdc08eb6e7068e61676e71c45
EA <pak_ea@inbox.ru>
2022-01-31 13:21:46 +0300

    formsets

--- static/js/order_scripts.js
+++ static/js/order_scripts.js
@@ -59,4 +59,18 @@ window.onload = function () {
         $('.order_total_quantity').html(order_total_quantity.toString());
         $('.order_total_cost').html(order_total_cost.toString() + ',00');
     }
+
+    $('.formset_row').formset({
+        addText : 'добавить продукт',
+        deleteText : 'удалить',
+        prefix : 'orderitems',
+        removed : deleteOrderItem,
+    });
+
+    function deleteOrderItem(row) {
+        let target_name = row[0].querySelector('input[type="number"]').name;
+        orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+        delta_quantity = -quantity_arr[orderitem_num];
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    }
 }
